e9629f0bb9b02b31c7e19d0cdb82417dd8500800
keith bonarrigo <keith@keithbonline.com>
2018-06-09 14:25:29 -0700

    adding inherited __repr__ functionality

--- block.py
+++ block.py
@@ -1,7 +1,7 @@
 from time import time
+from printable import Printable
 
-
-class Block:
+class Block(Printable):
 
     def __init__(self, index, previous_hash, transactions, proof, timestamp=None):
         self.index = index
@@ -9,4 +9,7 @@ class Block:
         self.timestamp = timestamp
         self.transactions = transactions
         self.proof = proof
+
+    def __repr__(self):
+        return 'Index: {}, Previous Hash: {}, Proof: {}, Transactions: {}'.format(self.index, self.previous_hash, self.proof, self.transactions)
 
